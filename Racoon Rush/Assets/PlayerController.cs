91f2fa19b3070275687851a5360665be8fa46681
Canyon1997 <canyon.davidson@gmail.com>
2020-10-14 12:13:47 -0400

    Prototype

---
+++ Racoon Rush/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    public float jumpForce;
+    public float rayLength;
+
+    public bool jump;
+    public bool isGrounded;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        RayCast();
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jump = true;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       if(jump)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void RayCast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength);
+
+        if(hit.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
