a8f8f3ec0bc9d559d303bf76a13e0a48846786f3
Canyon1997 <canyon.davidson@gmail.com>
2020-10-14 13:56:47 -0400

    Prototype update
    Jumping added

--- Racoon Rush/Assets/PlayerController.cs
+++ Racoon Rush/Assets/PlayerController.cs
@@ -6,11 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
 
-    public float jumpForce;
-    public float rayLength;
 
-    public bool jump;
-    public bool isGrounded;
+    public float jumpForce;
 
     private void Awake()
     {
@@ -19,34 +16,15 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        RayCast();
-
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            rb2d.velocity = Vector2.up * jumpForce;
         }
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-       if(jump)
-        {
-            rb2d.AddForce(Vector2.up * jumpForce);
-        }
-    }
 
-    private void RayCast()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength);
-
-        if(hit.collider.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
     }
 }
