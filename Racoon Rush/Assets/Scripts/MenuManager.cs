9c4f6eb9f86519934bff76102d5c66beaa320ecf
ProfessorChimp <40414867+ProfessorChimp@users.noreply.github.com>
2020-10-19 12:36:41 -0400

    UI Update #5
    Menus, god I hope this works

---
+++ Racoon Rush/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("SantisGameplay");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("I quit!!!");
+        Application.Quit();
+    }
+}
