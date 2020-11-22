1a6d3a9378ed194e119440a89f5676257bb1783a
Canyon1997 <canyon.davidson@gmail.com>
2020-11-22 18:57:07 -0500

    Trash can collider fix/Enemy obstacle rework

--- Racoon Rush/Assets/Scripts/Gameplay/EnemyAI.cs
+++ Racoon Rush/Assets/Scripts/Gameplay/EnemyAI.cs
@@ -21,4 +21,12 @@ public class EnemyAI : MonoBehaviour
         }
         
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("TrashCan"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 }
