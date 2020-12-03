737c51e6bd81364be1e7457c2fd71acbd89a43e3
ProfessorChimp <40414867+ProfessorChimp@users.noreply.github.com>
2020-12-03 00:11:46 -0500

    Updated UI Buttons & ChunkSpawner
    Re-implemented UI buttons and adjusted ChunkSpawner to properly spawn in GameChunk at the proper location and size

--- Racoon Rush/Assets/Scripts/Gameplay/ChunkSpawner.cs
+++ Racoon Rush/Assets/Scripts/Gameplay/ChunkSpawner.cs
@@ -8,18 +8,25 @@ public class ChunkSpawner : MonoBehaviour
 	[SerializeField] private GameObject chunkPrefab;
 	public GameObject gameChunkParent;
 
+	public float offset = 0;
+
+	public Transform lastPosition;
+
 	void Start()
     {
-
+       lastPosition = gameChunkParent.transform;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.tag == "Player")
 		{
-		Vector3 spawnLocation =  gameChunkParent.transform.position + new Vector3(21.6f, 0, 0);
+		Vector3 spawnLocation =  new Vector3(lastPosition.position.x + offset, -0.188f, 0);
 		var newChunk = Instantiate(chunkPrefab, spawnLocation, Quaternion.identity);
 		newChunk.name = "GameChunk";
+		lastPosition = newChunk.transform;
+		
+		
 		}
 	}
 }
