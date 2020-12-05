b6186170ff9cb5ca6df5074c4720c219c2ac1a8a
Canyon1997 <canyon.davidson@gmail.com>
2020-12-05 17:17:07 -0500

    Animations and collision fixes

--- Racoon Rush/Assets/Scripts/Gameplay/EnemyAI.cs
+++ Racoon Rush/Assets/Scripts/Gameplay/EnemyAI.cs
@@ -7,15 +7,67 @@ public class EnemyAI : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+    [SerializeField] private GameObject player;
+    [SerializeField] private PlayerController controller;
+
     [Header("Enemy")]
     [SerializeField] private float enemyMoveSpeed;
+    private Difficulty level;
+
+    private enum Difficulty
+    {
+        Level2,
+        Level3,
+        Level4
+    };
 
     private void Awake() => rb = GetComponent<Rigidbody2D>();
 
-    /*private void Update()
-	{
-		transform.Translate(Vector2.right * Time.deltaTime * movementSpeed);
-	}*/
+    private void Start()
+    {
+        controller = player.GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        //EnemyDifficutly();
+    }
+
+    /*private void EnemyDifficutly()
+    {
+        //Score relation to level difficulty
+        if (controller.score > 1000)
+        {
+            level = Difficulty.Level2;
+        }
+        else if(controller.score > 2500)
+        {
+            level = Difficulty.Level3;
+        }
+        else if(controller.score > 5000)
+        {
+            level = Difficulty.Level4;
+        }
+
+        //Speed multiplier to level difficulty
+        switch(level)
+        {
+            case Difficulty.Level2:
+                enemyMoveSpeed += 2;
+                controller.moveSpeed += 2;
+                break;
+
+            case Difficulty.Level3:
+                enemyMoveSpeed += 2;
+                controller.moveSpeed += 2;
+                break;
+
+            case Difficulty.Level4:
+                enemyMoveSpeed += 2;
+                controller.moveSpeed += 1;
+                break;
+        }
+    }*/
 
     private void FixedUpdate()
     {
@@ -27,14 +79,12 @@ public class EnemyAI : MonoBehaviour
         rb.AddForce(Vector2.right * enemyMoveSpeed);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.name == "Raccoon")
+        if(other.gameObject.CompareTag("Player"))
         {
-           Debug.Log("Hit Detected. You Lose!!");
-           SceneManager.LoadScene("SantisLoseScreen");
+            SceneManager.LoadScene("SantisLoseScreen");
         }
-        
     }
 
     private void OnTriggerExit2D(Collider2D other)
