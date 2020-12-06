840a1a6cec6f0c262ed08e710ca41c5189981915
ProfessorChimp <40414867+ProfessorChimp@users.noreply.github.com>
2020-12-06 16:35:08 -0500

    Back Button UI & Volume
    Added the Back Button UI sprites as well as a Volume slider with a MusicPlayer script

---
+++ Racoon Rush/Assets/Scripts/Menus/MusicPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour
+{
+    public AudioSource hipjazz;
+
+    private float musicVolume = 1f;
+    void Start()
+    {
+        hipjazz.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hipjazz.volume = musicVolume;
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
